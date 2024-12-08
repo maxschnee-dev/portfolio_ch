e959de2b8effafa83e3feaa1ac5691949390e71f
Max <maxschnee.dev@gmail.com>
2024-12-08 17:10:18 +0100

    Add Equans Swizerland cool af hover effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,24 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="p-5">
-      <h1 className="text-5xl sm:text-7xl">
-        Max<strong className="font-normal text-gray-400">imilian</strong>{" "}
-        Schneebeli
-      </h1>
+      <div className="flex flex-row justify-between">
+        <h1 className="text-5xl sm:text-7xl">
+          Max<strong className="font-normal text-gray-400">imilian</strong>{" "}
+          Schneebeli
+        </h1>
+        <select name="Lang" id="lang">
+          <option value="en">ENG</option>
+          <option value="de">DE</option>
+        </select>
+      </div>
 
-      <ul>
-        <li>
-          - Apprentice <b>Electrician</b>
+      <ul className="mt-2 sm:text-2xl">
+        <li className="group">
+          - <b>Electrical</b> Apprentice{" "}
+          <a href="https://www.equans.ch/home" className="inline-block">
+            @ Equans Swizerland
+            <span className="max-w-0 group-hover:max-w-full bg-opacity-0 group-hover:bg-opacity-100 transition-all delay-400 h-0.5 block translate-y-[-0.3rem] bg-black" />
+          </a>
         </li>
         <li>
           - <b>Programming</b> Hobbyist
