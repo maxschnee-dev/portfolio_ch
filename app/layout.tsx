438a315f6682ebb657b7469f41218fa290c8db41
Max <maxschnee.dev@gmail.com>
2024-12-09 00:12:30 +0100

    FEAT: Add self hosted monitoring with umami js

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* Self hosted Analytics */}
+      <script
+        defer
+        src="https://umami.max07.ch/script.js"
+        data-website-id="246a00fd-70ff-4a21-be73-bcfd5d1a120c"
+      ></script>
       <body className={inter.className}>{children}</body>
     </html>
   );
