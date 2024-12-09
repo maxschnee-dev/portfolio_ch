709ce0e2cec315a6d89b13fe59516b503c4f8eae
Max <maxschnee.dev@gmail.com>
2024-12-09 13:48:03 +0100

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const t = useTranslations("Landing");
 
   return (
-    <div className="p-5">
+    <div className="p-8">
       <div className="flex flex-row justify-between">
         <h1 className="text-5xl sm:text-7xl">
           Max<strong className="font-normal text-gray-400">imilian</strong>{" "}
@@ -18,7 +18,7 @@ export default function Home() {
 
       <ul className="mt-2 sm:text-2xl">
         <li>
-          - <RichText>{(tags) => t.rich("position", tags)}</RichText>
+          - <RichText>{(tags) => t.rich("position", tags)}</RichText>{" "}
           <a href="https://www.equans.ch/home" className="inline-block group">
             @ Equans Swizerland
             <span className="max-w-0 group-hover:max-w-full bg-opacity-0 group-hover:bg-opacity-100 transition-all delay-400 h-0.5 block translate-y-[-0.3rem] bg-black" />
